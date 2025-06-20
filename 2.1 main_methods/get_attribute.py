9b0ae69b014c8503ca2a65d5a32ad76805f0612f
denis sazonov <denis.sazonov@mytona.com>
2025-06-20 13:46:38 +0700

    NEXT: execute_script

--- 2.1 main_methods/get_attribute.py
+++ 2.1 main_methods/get_attribute.py
@@ -3,29 +3,21 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 try:
-    link = "https://suninjuly.github.io/get_attribute.html"
+    link = "https://suninjuly.github.io/selects1.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    treasure = browser.find_element(By.CSS_SELECTOR, "#treasure")
-    treasure_value = treasure.get_attribute("valuex")
+    num_1 = browser.find_element(By.ID, "num1")
+    num_2 = browser.find_element(By.ID, "num2")
+    sum = int(num_1.text) + int(num_2.text)
 
-    def calc(x):
-        return str(math.log(abs(12 * math.sin(int(x)))))
-    y = calc(treasure_value)
+    dropdown = Select(browser.find_element(By.ID, "dropdown"))
+    dropdown.select_by_value(str(sum))
 
-    answer = browser.find_element(By.XPATH, "//input[@id='answer']")
-    answer.send_keys(y)
-
-    checkbox = browser.find_element(By.XPATH, "//input[@id='robotCheckbox']")
-    checkbox.click()
-
-    radiobutton = browser.find_element(By.XPATH, "//input[@id='robotsRule']")
-    radiobutton.click()
-
-    submit_button = browser.find_element(By.XPATH, "//button[@type='submit']")
+    submit_button = browser.find_element(By.XPATH, "//button[normalize-space()='Submit']")
     submit_button.click()
 
 finally:
--- 2.2 work_with_files/example.py
+++ 2.2 work_with_files/example.py
@@ -1,1 +1,35 @@
-# NEXT
+import math
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+try:
+    link = "https://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    treasure = browser.find_element(By.CSS_SELECTOR, "#treasure")
+    treasure_value = treasure.get_attribute("valuex")
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+    y = calc(treasure_value)
+
+    answer = browser.find_element(By.XPATH, "//input[@id='answer']")
+    answer.send_keys(y)
+
+    checkbox = browser.find_element(By.XPATH, "//input[@id='robotCheckbox']")
+    checkbox.click()
+
+    radiobutton = browser.find_element(By.XPATH, "//input[@id='robotsRule']")
+    radiobutton.click()
+
+    submit_button = browser.find_element(By.XPATH, "//button[@type='submit']")
+    submit_button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
